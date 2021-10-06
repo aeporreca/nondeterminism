5728e9cb90b9c203bfac389713ba9628611d7598
Antonio E. Porreca <antonio.porreca@lis-lab.fr>
2021-10-06 17:19:50 +0200

    Reorganised the examples and updated the README

---
+++ prime.py
@@ -0,0 +1,25 @@
+from nondeterminism import *
+
+
+@nondeterministic
+def composite(n):
+    d = guess(range(2, n))
+    if n % d == 0:
+        accept()
+    else:
+        reject()
+
+
+def prime(n):
+    if n < 2:
+        return False
+    else:
+        return not composite(n)
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for n in numbers:
+    if prime(n):
+        print('The integer', n, 'is prime')
+    else:
+        print('The integer', n, 'is not prime')
---
+++ sat.py
@@ -0,0 +1,24 @@
+from nondeterminism import *
+
+
+@nondeterministic
+def satisfiable(formula):
+    x = guess()
+    y = guess()
+    z = guess()
+    if formula(x, y, z):
+        accept()
+    else:
+        reject()
+
+
+def formula(x, y, z):
+    return ((x or y) and
+            (y or z) and
+            (not x or z))
+
+
+if satisfiable(formula):
+    print('The formula is satisfiable')
+else:
+    print('The formula is not satisfiable')
