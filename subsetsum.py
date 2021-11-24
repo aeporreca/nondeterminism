2c6a88d76cd864ba8318dd9b81b47c4685559947
Antonio E. Porreca <antonio.porreca@lis-lab.fr>
2021-11-24 18:10:13 +0100

    Reworked the example

--- subsetsum.py
+++ subsetsum.py
@@ -4,14 +4,14 @@ from nondeterminism import *
 @nondeterministic
 def subset_sum(values, target):
     subset = set()
-    for x in values:
+    for item in values:
         if guess():
-            subset.add(x)
+            subset.add(item)
     if sum(subset) == target:
         return subset
 
 
-values = {1, 2, 3, 5}
+values = {1, 4, 5}
 for target in range(10):
     result = subset_sum(values, target)
     print(f'A subset {values} having sum {target} is:', result)
