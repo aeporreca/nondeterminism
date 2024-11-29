46d764d648b781f805cc66eb08735de19a700915
Antonio E. Porreca <antonio.porreca@lis-lab.fr>
2024-11-29 08:35:20 +0100

    Changed the interface once again: since start is needed, inject is not, and combine can do all the work; see, e.g., Haskell's foldl

--- nondeterminism.py
+++ nondeterminism.py
@@ -11,48 +11,44 @@ __all__ = [
 import functools as ft
 import multiprocessing as mp
 import os
-import operator as op
 
 
-# Default combine, inject and shortcircuit for nondeterminize
+# Default values for combine, shortcircuit, and postprocess for
+# nondeterminize
 
 def disjunction(x, y):
     return x or y
 
+def is_success(x):
+    return x is not None and x is not False
 
 def identity(x):
     return x
 
 
-def never(x):
-    return False
-
-
 # Modify function so that it returns the result of accumulating by
-# combine the injection of the return values of the computations of
-# function, starting from start and stopping when shorcircuit by stop;
-# finally, return postprocess applied to the accumulator
+# combine the return values of the computations of function, starting
+# from start and stopping early if indicated by shortcircuit; finally,
+# return apply postprocess to the accumulator
 
 def nondeterminize(function, combine=disjunction, start=None,
-                   inject=identity, shortcircuit=never,
-                   postprocess=identity):
+                   shortcircuit=is_success, postprocess=identity):
     @ft.wraps(function)
     def wrapper(*args, **kwargs):
         queue = mp.SimpleQueue()
         queue.put(start)                          # Initial accumulator
         if os.fork() == 0:
-            result = function(*args, **kwargs)    # Actually execute function
-            acc = queue.get()                     # Get the accumulator
-            acc = combine(acc, inject(result))    # Update it
-            print(acc)
+            res = function(*args, **kwargs)       # Actually execute function
+            acc = queue.get()                     # Get the accumulated result
+            acc = combine(acc, res)               # Update it
             queue.put(acc)                        # Replace the accumulator
-            if shortcircuit(acc):                 # Stop here
+            if shortcircuit(acc):                 # We can stop here
                 os._exit(0)                       # No need to go on
             else:
                 os._exit(1)                       # Keep on computing
         else:
             os.wait()                             # No need to check the status
-            return postprocess(queue.get())       # Postprocessing
+            return postprocess(queue.get())       # Optional postprocessing
     return wrapper
 
 
@@ -70,67 +66,61 @@ def guess(choices=(False, True)):
     os._exit(1)                                   # Exhausted all choices
 
 
-# Deterministic decorator
 
-def always(x):
+# Nondeterministic decorator
+
+def nondeterministic(function):
+    return nondeterminize(function)
+
+
+# Deterministic decorator: make the function... deterministic, i.e.,
+# return the first result, successful or not (useless but it shows
+# that this library covers this trivial case too)
+
+def always(_):
     return True
-
 
 def deterministic(function):
     return nondeterminize(function, shortcircuit=always)
 
 
-# Noneterministic decorator
-
-def is_success(x):
-    return x is not None and x is not False
-
-
-def nondeterministic(function):
-    return nondeterminize(function, shortcircuit=is_success)
-
-
-# Cononeterministic decorator
+# Conondeterministic decorator
 
 def conjunction(x, y):
     return x and y
 
-
 def is_failure(x):
     return x is None or x is False
-
 
 def conondeterministic(function):
     return nondeterminize(function, combine=conjunction,
                           start=True, shortcircuit=is_failure)
 
 
-# Counting decorator
+# Counting decorator: make the function return the number of accepting
+# computations
 
-def binarize(x):
-    return int(x is not None and x is not False)
+def add(acc, res):
+    return acc + int(is_success(res))
 
+def never(_):
+    return False
 
 def counting(function):
-    return nondeterminize(function, combine=op.add,
-                          start=0, inject=binarize)
+    return nondeterminize(function, combine=add,
+                          start=0, shortcircuit=never)
 
 
-# Majority decorator
+# Majority decorator: make the function return true iff the majority
+# of its computations are accepting
 
-def tuple_add(x, y):
-    return (x[0] + y[0], x[1] + y[1])
-
-
-def binarize_and_count(x):
-    return (binarize(x), 1)
-
+def add_counting(acc, res):
+    return (acc[0] + int(is_success(res)), acc[1] + 1)
 
 def check_majority(acc):
     return 2 * acc[0] > acc[1]
 
-
 def majority(function):
-    return nondeterminize(function, combine=tuple_add,
-                          start=(0, 0), inject=binarize_and_count,
+    return nondeterminize(function, combine=add_counting,
+                          start=(0, 0), shortcircuit=never,
                           postprocess=check_majority)
