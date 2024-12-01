7a9f6ab9f39c472dee1f6975470c878ef6a28e95
Antonio E. Porreca <antonio.porreca@lis-lab.fr>
2024-12-01 18:22:13 +0100

    Completely new version, supports alternation

--- nondeterminism.py
+++ nondeterminism.py
@@ -1,10 +1,10 @@
+from __future__ import annotations
+
+
 __all__ = [
-    'deterministic',
-    'nondeterministic',
-    'conondeterministic',
-    'counting',
-    'majority',
-    'guess',
+    'deterministic', 'guess', 'coguess',
+    'Or', 'And', 'Count', 'Majority', 'Maximize',
+    'is_leaf', 'is_true',
 ]
 
 
@@ -12,114 +12,118 @@ import functools as ft
 import multiprocessing as mp
 import os
 
-
-# Default values for combine, shortcircuit, and postprocess for
-# nondeterminize
-
-def disjunction(x, y):
-    return x or y
-
-def is_success(x):
-    return x is not None and x is not False
-
-def identity(x):
-    return x
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from typing import Callable
 
 
-# Modify function so that it returns the result of accumulating by
-# combine the return values of the computations of function, starting
-# from start and stopping early if indicated by shortcircuit; finally,
-# return apply postprocess to the accumulator
+@dataclass
+class CompTree[T](ABC):
+    children: list[CompTree[T] | T]
 
-def nondeterminize(function, combine=disjunction, start=None,
-                   shortcircuit=is_success, postprocess=identity):
+    @abstractmethod
+    def eval(self):
+        ...
+
+
+def is_leaf(tree):
+    return not isinstance(tree, CompTree)
+
+
+def eval(result):
+    if is_leaf(result):
+        return result
+    else:
+        return result.eval()
+        
+
+class Or(CompTree):
+    def __bool__(self):
+        return any(self.children)
+
+    def eval(self):
+        return bool(self)
+            
+
+class And(CompTree):
+    def __bool__(self):
+        return all(self.children)
+
+    def eval(self):
+        return bool(self)
+
+
+class CountingCompTree(CompTree):
+    def count(self, pred):
+        cnt = pred(self)
+        for child in self.children:
+            if is_leaf(child):
+                cnt += int(pred(child))
+            else:
+                cnt += child.count(pred)
+        return cnt
+
+
+def is_true(x):
+    return x is True
+
+
+class Count(CountingCompTree):
+    def eval(self):
+        return self.count(is_true)
+
+
+class Majority(CountingCompTree):
+    def eval(self):
+        n_leaves = self.count(is_leaf)
+        n_true = self.count(is_true)
+        return 2 * n_true > n_leaves
+
+
+@dataclass
+class Maximize_[T](CompTree):
+    function: Callable[[T], int | float]
+
+    def eval(self):
+        domain = (eval(child) for child in self.children)
+        return max(domain, key=self.function)
+
+
+def Maximize(function):
+    def partial(children):
+        return Maximize_(children, function)
+    return partial
+
+
+RESULT = mp.SimpleQueue()
+
+
+def nondeterministic(function):
     @ft.wraps(function)
     def wrapper(*args, **kwargs):
-        queue = mp.SimpleQueue()
-        queue.put(start)                          # Initial accumulator
         if os.fork() == 0:
-            res = function(*args, **kwargs)       # Actually execute function
-            acc = queue.get()                     # Get the accumulated result
-            acc = combine(acc, res)               # Update it
-            queue.put(acc)                        # Replace the accumulator
-            if shortcircuit(acc):                 # We can stop here
-                os._exit(0)                       # No need to go on
-            else:
-                os._exit(1)                       # Keep on computing
+            result = function(*args, **kwargs)
+            RESULT.put(result)
+            os._exit(0)
         else:
-            os.wait()                             # No need to check the status
-            return postprocess(queue.get())       # Optional postprocessing
+            os.wait()
+            result = RESULT.get()
+            return eval(result)
     return wrapper
 
 
-# Guess one of the choices, halting as soon as a computation
-# accepts (i.e., it returns non-False, non-None)
-
-def guess(choices=(False, True)):
+def guess(choices=(False, True), *, mode=Or):
+    children = []
     for choice in choices:
         if os.fork() == 0:
             return choice
         else:
-            _, status = os.wait()
-            if os.WEXITSTATUS(status) == 0:       # Found an acceptable result
-                os._exit(0)                       # No need to go on
-    os._exit(1)                                   # Exhausted all choices
+            os.wait()
+            result = RESULT.get()
+            children.append(result)
+    RESULT.put(mode(children))
+    os._exit(0)
 
 
-# Nondeterministic decorator
-
-def nondeterministic(function):
-    return nondeterminize(function)
-
-
-# Deterministic decorator: make the function... deterministic, i.e.,
-# return the first result, successful or not (useless but it shows
-# that this library covers this trivial case too)
-
-def always(_):
-    return True
-
-def deterministic(function):
-    return nondeterminize(function, shortcircuit=always)
-
-
-# Conondeterministic decorator
-
-def conjunction(x, y):
-    return x and y
-
-def is_failure(x):
-    return x is None or x is False
-
-def conondeterministic(function):
-    return nondeterminize(function, combine=conjunction,
-                          start=True, shortcircuit=is_failure)
-
-
-# Counting decorator: make the function return the number of accepting
-# computations
-
-def add(acc, res):
-    return acc + int(is_success(res))
-
-def never(_):
-    return False
-
-def counting(function):
-    return nondeterminize(function, combine=add,
-                          start=0, shortcircuit=never)
-
-
-# Majority decorator: make the function return true iff the majority
-# of its computations are accepting
-
-def add_counting(acc, res):
-    return (acc[0] + int(is_success(res)), acc[1] + 1)
-
-def check_majority(acc):
-    return 2 * acc[0] > acc[1]
-
-def majority(function):
-    return nondeterminize(function, combine=add_counting,
-                          start=(0, 0), shortcircuit=never,
-                          postprocess=check_majority)
+def coguess(choices=(False, True)):
+    return guess(choices, mode=And)
